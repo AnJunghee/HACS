01e036f55526e9256f6ad7329ddb99943691084e
yeon <dalcon10280@gmail.com>
2021-01-18 18:48:49 +0900

    [ADD] 404에러 페이지 추가

--- src/router/index.js
+++ src/router/index.js
@@ -43,6 +43,10 @@ const routes = [
     name: 'hr_emp',
     component: () => import('@/views/hr/HrEmp'),
   },
+  {
+    path: '*',
+    component: () => import('@/views/PageNotFound'),
+  },
 ]
 
 const router = new VueRouter({
