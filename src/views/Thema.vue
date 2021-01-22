d5505c58eb0689692ab0e959ba0117e91e0a44be
DESKTOP-50SDCI9\User <daepyo5@naver.com>
2021-01-22 11:45:46 +0900

    [ADD] 메인 페이지 추가

--- src/views/Thema.vue
+++ src/views/Thema.vue
@@ -1,7 +1,46 @@
 <template>
   <login-box :width="344">
+
     <template v-slot:content>
-      <logo></logo>
+    
+        <div style="width=75px">
+          <logo></logo>
+        </div>
+        <p>안녕하세요</p>
+        <v-btn
+          outlined
+          plain
+          @click="btnLogout"
+        >로그아웃</v-btn>
+   
+
+      <v-form>
+        <div>
+          <v-btn
+            outlined
+            plain
+            @click="btn1"
+          >인사</v-btn>
+          <v-btn
+            outlined
+            plain
+            @click="btn2"
+          >회계</v-btn>
+        </div>
+        <div>
+          <v-btn
+            outlined
+            plain
+            @click="btn3"
+          >편의</v-btn>
+          <v-btn
+            outlined
+            plain
+            @click="btn4"
+          >시스템관리</v-btn>
+        </div>
+      </v-form>
+
     </template>
   </login-box>
 </template>
@@ -14,6 +53,23 @@ export default {
   name: 'thema',
   components: {
     Logo, LoginBox,
+  },
+  methods : {
+    btnLogout() {
+      this.$router.push('Login')
+    },
+    btn1(){
+      this.$router.push('Login')
+    },
+    btn2(){
+      this.$router.push('Login')
+    },
+    btn3(){
+      this.$router.push('Login')
+    },
+    btn4(){
+      this.$router.push('Login')
+    },
   },
 }
 </script>
