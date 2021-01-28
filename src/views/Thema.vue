8e1ea9c973c1b85195cd4b3bed758cda2263cdaf
DESKTOP-50SDCI9\User <daepyo5@naver.com>
2021-01-28 15:13:46 +0900

    [MOD] 메인 페이지 수정

--- src/views/Thema.vue
+++ src/views/Thema.vue
@@ -1,58 +1,60 @@
 <template>
-  <login-box :width="344">
+  <login-box :width="1000">
 
     <template v-slot:content>
-    
-        <div style="width=75px">
-          <logo></logo>
+        
+        <div style='width:250px; float:right'>
+          <span>안녕하세요 ㅇㅇ님 </span>
+          <v-btn
+            outlined
+            @click="btnLogout"
+          >로그아웃</v-btn>
         </div>
-        <p>안녕하세요</p>
-        <v-btn
-          outlined
-          plain
-          @click="btnLogout"
-        >로그아웃</v-btn>
-   
 
-      <v-form>
+        <br>
+        <br>
+
         <div>
           <v-btn
             outlined
-            plain
+            height="300px"
+            width="484px"
             @click="btn1"
           >인사</v-btn>
           <v-btn
             outlined
-            plain
+            height="300px"
+            width="484px"
             @click="btn2"
           >회계</v-btn>
-        </div>
-        <div>
           <v-btn
             outlined
-            plain
+            height="300px"
+            width="484px"
             @click="btn3"
-          >편의</v-btn>
+          >편의</v-btn> 
           <v-btn
             outlined
-            plain
+            height="300px"
+            width="484px"
             @click="btn4"
           >시스템관리</v-btn>
+          
         </div>
-      </v-form>
+
 
     </template>
   </login-box>
 </template>
 
 <script>
-import Logo from '@/components/Logo'
+
 import LoginBox from '@/components/LoginBox'
 
 export default {
   name: 'thema',
   components: {
-    Logo, LoginBox,
+   LoginBox,
   },
   methods : {
     btnLogout() {
